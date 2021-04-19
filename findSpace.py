fdf9585144b2fa08f0a2f3f379446413d57e219f
himanshikohli19 <himanshi.kohli19@gmail.com>
2021-04-19 12:30:29 +0530

    using find function in a string

---
+++ findSpace.py
@@ -0,0 +1,4 @@
+# this program returns the index number having double spaces in a sentence..
+str1="This program  returns the index number  having double spaces in a sentence.."
+print("Actual string:\n", str1)
+print(str1.find("  "))
