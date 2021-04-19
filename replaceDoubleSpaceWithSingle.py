7c46ab799e70ac7fbb656fe8443ad931e84818cd
himanshikohli19 <himanshi.kohli19@gmail.com>
2021-04-19 12:32:00 +0530

    using replace function

---
+++ replaceDoubleSpaceWithSingle.py
@@ -0,0 +1,4 @@
+# this program replace double spaces with a single space in a sentence..
+str1="a string with  double space will be replaced by a single space"
+print(str1)
+print(str1.replace("  "," "))
