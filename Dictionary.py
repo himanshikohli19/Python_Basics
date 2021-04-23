925cde22fba40f548c186b0f32513f7c552ebd54
himanshikohli19 <himanshi.kohli19@gmail.com>
2021-04-23 12:35:07 +0530

    Testing Dictionary functions

---
+++ Dictionary.py
@@ -0,0 +1,7 @@
+dict1={"Name":"Himanshi","list1":{1,2,3,4},"tuple1":(2,3,4,5),"test1":1}
+print(type(dict1["list1"]))
+#testing all dictionary functions
+print(dict1.items())
+print(dict1.keys())
+print(dict1.values())
+print(dict1.get("Name"))
